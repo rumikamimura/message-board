5b5b945ec81d938bca3d09da9f4cf3a18cd71bf3
rumikamimura <runrun_and_ruri2002@yahoo.co.jp>
2015-10-17 22:45:51 +0000

    techaca8.6

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_action :set_message, only: [:edit, :update]
+  
   def index
      # Messageを全て取得する。
     @messages = Message.all
@@ -17,10 +19,31 @@ class MessagesController < ApplicationController
       render 'index'
     end
   end
+  
+  ### ここからさらに追記 8.4
+  def edit
+  end
+  
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  ### ここまで 8.4
 
   private
   def message_params
     params.require(:message).permit(:name, :body)
   end
   ## ここまで
+  
+  ### ここからさらに追記 8.4
+  def set_message
+    @message = Message.find(params[:id])
+  end
+  ### ここまで 8.4
 end
